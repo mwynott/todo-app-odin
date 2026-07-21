e2c7956102db0ccd0d08a98aae76946bbe4a6d15
mwynott <mwynott1989@gmail.com>
2026-07-21 17:41:22 -0400

    Add localStorage logic and export function shells for render.js

--- src/index.js
+++ src/index.js
@@ -2,8 +2,30 @@ import { TodoList } from "./todoList.js";
 import { seedProjects } from "./seedProjects.js";
 import { Project } from "./project.js";
 import { Todo } from "./todo.js";
+import { renderTodoList, renderProject, renderTodo } from "./render.js";
 
-const todoList = new TodoList("My Todo List", seedProjects);
-console.log(todoList);
-console.log("Initial Projects:", seedProjects);
+function saveTodoList(todoList) {
+    localStorage.setItem("todoList", JSON.stringify(todoList));
+}
+
+function loadTodoList() {
+    const rawData = JSON.parse(localStorage.getItem("todoList"));
+    if (!rawData) return new TodoList("My Todo List", seedProjects);
+
+    const projects = rawData.projects.map(p =>
+        new Project(p.name, p.todos.map(t =>
+            new Todo(t.title, t.description, t.dueDate, t.notes, t.priority, t.completed)
+        ))
+    );
+    return new TodoList(rawData.name, projects);
+}
+
+function refresh() {
+    saveTodoList(todoList);
+    const app = document.getElementById("app");
+    app.innerHTML = "";
+    app.append(renderTodoList(todoList));
+}
+
+const todoList = loadTodoList();
 
---
+++ src/render.js
@@ -0,0 +1,4 @@
+//module that handles rendering of the page
+export function renderTodoList(todoList, onChange) {}
+export function renderProject(project, todoList, onChange) {}
+export function renderTodo(todo, project, todoList, onChange) {}
