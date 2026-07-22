89bec3aa3683edc1fd11c9d46ca0030b75ace607
mwynott <mwynott1989@gmail.com>
2026-07-22 17:13:15 -0400

    Add function to render todos with li and button

--- src/render.js
+++ src/render.js
@@ -1,4 +1,21 @@
 //module that handles rendering of the page
+
+
+
 export function renderTodoList(todoList, onChange) {}
 export function renderProject(project, todoList, onChange) {}
-export function renderTodo(todo, project, todoList, onChange) {}
+
+export function renderTodo(todo, project, todoList, onChange) {
+    const li = document.createElement("li");
+    li.textContent = `{todo.title} ({todo.priority})`;
+
+    const completeBtn = document.createElement("button");
+    completeBtn.textContent = todo.completed ? "Undo" : "Complete";
+        completeBtn.addEventListener("click", () =>
+            project.markTodoCompleted(todo.title);
+            onChange(); //re-render after changes are made
+        );
+        
+        li.append(completeBtn);
+        return li;
+}
